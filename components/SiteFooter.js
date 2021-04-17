f7bcbd72c3040353a4f0984241ac2d0acd8bfb98
adil-82 <bouaddiadil82@gmail.com>
2021-04-17 16:22:46 +0200

    Site Footer

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -1,7 +1,11 @@
 export default function SiteFooter() {
     return (
-            <div>
-                Site Footer
+            <div className="bg-blue-900 py-3 text-sm text-center text-blue-300 font-semibold">
+                Hosted on {""}
+                <a href="http://google.com" className="text-blue-100 underline hover:text-white ">
+                     DigitalOcean App Platform
+                </a>
+                 
             </div>
     )
 }
